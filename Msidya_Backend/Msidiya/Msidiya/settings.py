05d4c20e0a1e6b4f6710efed41dd442fea4ae65e
ali_maameri02 <ali.maameri@univ-constanine2.com>
2024-10-17 23:29:11 +0100

    settings merge

--- Msidya_Backend/Msidiya/Msidiya/settings.py
+++ Msidya_Backend/Msidiya/Msidiya/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Msidiya.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Msidiya',
+        'USER': 'postgres',
+        'PASSWORD': '40603010',
+        'HOST': 'localhost',  # or the IP address of your PostgreSQL server
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
 
